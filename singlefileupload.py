35904c7b2ab4cd3cda6be4b83f25aadd00e60e3c
Dan Publiweb <publiweb@Dan-Publiweb.local>
2022-07-13 18:10:33 +0300

    static

--- singlefileupload.py
+++ singlefileupload.py
@@ -16,7 +16,7 @@ import tempfile
 import glob
 
 
-app=Flask(__name__)
+app=Flask(__name__, static_folder='../static', static_url_path='/')
 app.secret_key = "secret key"
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
