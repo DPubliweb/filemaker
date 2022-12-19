533494054e8f1317302a0c8473dbde00733b321b
Dan Publiweb <91180870+DPubliweb@users.noreply.github.com>
2022-12-19 15:04:00 +0200

    Update singlefileupload.py

--- singlefileupload.py
+++ singlefileupload.py
@@ -13,7 +13,6 @@ import zipfile
 from io import BytesIO
 import requests
 
-
 app=Flask(__name__, static_folder='./static', static_url_path='/')
 app.secret_key = "secret key"
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
