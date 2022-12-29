5ba31e6b4726f71aa8fd2ad24a00a00dba016f2f
Dansultan <d.sultan147@gmail.com>
2022-12-29 20:33:02 +0200

    csv after sms

--- singlefileupload.py
+++ singlefileupload.py
@@ -203,200 +203,14 @@ def upload_file():
                     print("Salut",file)
             print("Salut", filenames)
             print('all file finish')
-       
+
 
         return render_template("content.html")
-    
+
     else:
         flash('Allowed file types are only csv')
         return redirect(request.url)
 
-
-@app.route('/mms', methods=['GET','POST'])
-def mms():
-    if request.method == 'POST':
-        render_template('content.html')
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        utm = request.form['utm']
-        if file.filename == '':
-            flash('No file selected for uploading')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            file = open(app.config['UPLOAD_FOLDER'] + '/'+filename,"r")
-            csv_reader_all = csv.reader(open(app.config['UPLOAD_FOLDER'] + '/'+filename, 'r', encoding='UTF-8'), delimiter=';')
-            count = 0
-            flag_input = request.form['flag']
-            name = request.form['name']
-            flag = int(flag_input)
-
-            line_count = 0
-
-            workbook = xlsxwriter.Workbook(os.path.abspath('parsed/'+name+'-p1.xlsx'))
-            worksheet = workbook.add_worksheet()
-
-            workbook1 = xlsxwriter.Workbook(os.path.abspath('parsed/'+name+'-p2.xlsx'))
-            worksheet1 = workbook1.add_worksheet()
-
-            workbook2 = xlsxwriter.Workbook(os.path.abspath('parsed/'+name+'-p3.xlsx'))
-            worksheet2 = workbook2.add_worksheet()
-
-            workbook3 = xlsxwriter.Workbook(os.path.abspath('parsed/'+name+'-p4.xlsx'))
-            worksheet3 = workbook3.add_worksheet()
-
-            for line in csv_reader_all:
-             if(count == 0):
-                first_line = line
-                #writer.writerow(line)
-                worksheet.write(0,0,first_line[0])
-                worksheet.write(0,1, first_line[1])
-                worksheet.write(0,2, first_line[2])
-                worksheet.write(0,3, first_line[3])
-                worksheet.write(0,4, first_line[4])
-                worksheet.write(0,5, first_line[5])
-                worksheet.write(0,6, first_line[6])
-                worksheet.write(0,7, first_line[7])
-
-             else:
-                if line[5] == 'Mme':
-                    line[5] = line[5]
-                elif line[5] == 'm':
-                    line[5] = 'M'
-                elif line[5] == 'f':
-                    line[5] = 'Mme'
-                else:
-                    line[5] = 'M'
-        
-        
-                line[3] = str(line[3])
-        
-                line[0] = line[0].replace("√®",'è')
-                line[0] = line[0].replace("√©", 'é')
-                line[0] = line[0].replace("√´", 'ë')
-                line[0] = line[0].replace("√ß", 'ç')
-                line[0] = line[0].replace("√™", 'ê')
-                line[0] = line[0].replace("√£¬ß", 'ç')
-                line[0] = line[0].replace("√Ø", 'ï')
-
-
-                line[1] = line[1].replace("√®", 'è')
-                line[1] = line[1].replace("√©", 'é')
-                line[1] = line[1].replace("√´", 'ë')
-                line[1] = line[1].replace("√ß", 'ç')
-                line[1] = line[1].replace("√™", 'ê')
-                line[1] = line[1].replace("√£¬ß", 'ç')
-                line[1] = line[1].replace("√Ø", 'ï')
-        
-                S = 5  # number of characters in the string.
-                # call random.choices() string module to find the string in Uppercase + numeric data.
-        
-                ran = ''.join(random.choices(string.ascii_uppercase + string.digits, k=S))
-                code = ran.replace ("0", "3")
-                line[7] = str(code)
-                if flag > 0:
-                    line[6] = '46.'+utm+'.p'+str(flag)
-                else:
-                    line[6] = utm
-                line[4] = "https://contact788081.typeform.com/to/uOCz2qY8?utm_source="+line[6]+"&name="+line[0]+"&surname="+line[1]+"&email="+line[2]+"&phone="+line[3]+"&code="+line[7]
-
-                line[1] = line[2] = ""
-
-                if count < 50001 :
-                    worksheet.write(line_count, 0, line[0])
-                    worksheet.write(line_count, 1, line[1])
-                    worksheet.write(line_count, 2, line[2])
-                    worksheet.write(line_count, 3, line[3])
-                    worksheet.write(line_count, 4, line[4])
-                    worksheet.write(line_count, 5, line[5])
-                    worksheet.write(line_count, 6, line[6])
-                    worksheet.write(line_count, 7, line[7])
-                elif count > 50001 and count < 100001:
-                    worksheet1.write(line_count, 0, line[0])
-                    worksheet1.write(line_count, 1, line[1])
-                    worksheet1.write(line_count, 2, line[2])
-                    worksheet1.write(line_count, 3, line[3])
-                    worksheet1.write(line_count, 4, line[4])
-                    worksheet1.write(line_count, 5, line[5])
-                    worksheet1.write(line_count, 6, line[6])
-                    worksheet1.write(line_count, 7, line[7])
-                elif count > 100001 and count < 150001:
-                    worksheet2.write(line_count, 0, line[0])
-                    worksheet2.write(line_count, 1, line[1])
-                    worksheet2.write(line_count, 2, line[2])
-                    worksheet2.write(line_count, 3, line[3])
-                    worksheet2.write(line_count, 4, line[4])
-                    worksheet2.write(line_count, 5, line[5])
-                    worksheet2.write(line_count, 6, line[6])
-                    worksheet2.write(line_count, 7, line[7])
-                elif count > 150001 and count < 200001:
-                    worksheet3.write(line_count, 0, line[0])
-                    worksheet3.write(line_count, 1, line[1])
-                    worksheet3.write(line_count, 2, line[2])
-                    worksheet3.write(line_count, 3, line[3])
-                    worksheet3.write(line_count, 4, line[4])
-                    worksheet3.write(line_count, 5, line[5])
-                    worksheet3.write(line_count, 6, line[6])
-                    worksheet3.write(line_count, 7, line[7])
-
-                if count%50000 == 0:
-                    flag = flag + 1
-                    line_count = 0
-        
-             count = count + 1
-             line_count = line_count +1
-             count_str = str(count)
-            print(count)
-            if count <= 50001 :
-                workbook.close()
-            elif count > 50001 and count <= 100001:
-                workbook.close()
-                workbook1.close()
-            elif count > 100001 and count <= 150001:
-                workbook.close()
-                workbook1.close()
-                workbook2.close()
-            elif count > 150001 and count <= 200001:
-                workbook.close()
-                workbook1.close()
-                workbook2.close()
-                workbook3.close()
-
-            
-            filenames = next(walk(os.path.abspath("parsed")), (None, None, []))[2]  # [] if no file
-            print(filenames)
-            count = 0
-            for file in filenames:
-                if (file != ".DS_Store"):
-                    count = count + 1
-                    print("start " + file)
-                    sample_file = open("parsed/" + file, "rb")
-                    upload_file = {"xlsxFile": sample_file}
-                    r = requests.post("https://sma.vc/upload-file", files=upload_file)
-                    if r.status_code == 200:
-                        print("finish parsed_" + file)
-                        with open(os.path.abspath("parsed/"+file), "wb") as f:
-                            f.write(r.content)
-                    elif r.status.code == 502:
-                        time.sleep(2)
-                        print("start " + file)
-                        sample_file = open("parsed/" + file, "rb")
-                        upload_file = {"xlsxFile": sample_file}
-                        r = requests.post("https://sma.vc/upload-file", files=upload_file)
-                else:
-                    print(file)
-            print('all file finish')
-       
-
-        return render_template("content.html")
-        
-    else:
-        flash('Allowed file types are only csv')
-        return redirect(request.url)
 
 @app.route('/zipped_data')
 def zipped_data():
@@ -405,7 +219,7 @@ def zipped_data():
     fileName = "parsed_files{}.zip".format(timestr)
     memory_file = BytesIO()
     file_path = os.path.abspath("ready")
-    
+
     with zipfile.ZipFile(memory_file, 'w', zipfile.ZIP_DEFLATED) as zipf:
           for root, dirs, files in os.walk(file_path):
                     for file in files:
@@ -450,6 +264,13 @@ def zipped_data_2():
                             zipf.write(os.path.join(root, file))
                             #os.remove(file_path)
     memory_file.seek(0)
+
+    filenames = next(walk(os.path.abspath("excel")), (None, None, []))[2]  # [] if no file
+    for file in filenames:
+                if (file != ".DS_Store"):
+                    file_path_del = (os.path.abspath("excel/"+file))
+                    os.remove(file_path_del)
+
     filenames2 = next(walk(os.path.abspath("final")), (None, None, []))[2]  # [] if no file
     for file in filenames2:
                 if (file != ".DS_Store"):
@@ -496,7 +317,7 @@ def sms_write():
             lien = "{lien}"
             print(short_url)
 
-            workbook = xlsxwriter.Workbook(os.path.abspath('final/'+name+'.xlsx'))
+            workbook = xlsxwriter.Workbook(os.path.abspath('excel/'+name+'.xlsx'))
             worksheet = workbook.add_worksheet()
 
             for line in csv_reader_all:
@@ -534,6 +355,14 @@ def sms_write():
              count_str = str(count)
             print(count)
             workbook.close()
+            filenames = next(walk(os.path.abspath("excel")), (None, None, []))[2]  # [] if no file
+            for file in filenames:
+                if (file != ".DS_Store"):
+                    read_file = pd.read_excel('excel/'+file)
+                    file = file[:-4]
+                    read_file.to_csv("final/"+file+"csv", index=None, header=True)
+            print("Salut", filenames)
+            print('all file finish')
 
         return render_template("content_sms.html")
         
