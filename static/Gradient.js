61c58615fb3423fbbad6ef61279afbd6026022bb
Dan Publiweb <91180870+DPubliweb@users.noreply.github.com>
2023-05-31 16:23:40 +0300

    grad

--- static/Gradient.js
+++ static/Gradient.js
@@ -11,7 +11,7 @@ function normalizeColor(hexCode) {
     return [(hexCode >> 16 & 255) / 255, (hexCode >> 8 & 255) / 255, (255 & hexCode) / 255]
   } ["SCREEN", "LINEAR_LIGHT"].reduce((hexCode, t, n) => Object.assign(hexCode, {
     [t]: n
-  }), {});
+  }), {}) ;
   
   //Essential functionality of WebGl
   //t = width
